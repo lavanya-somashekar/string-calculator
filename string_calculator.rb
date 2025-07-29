c60527becf388d9dfb074b5456265b26ab28f51d
slavanya <slavanya@plansource.com>
2025-07-29 14:26:29 +0530

    Implement: handle new lines b/w numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,10 +1,7 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers == ""
-    return numbers.to_i if numbers =~ /^\d+$/
-    if numbers.include?(',')
-      nums = numbers.split(',').map(&:to_i)
-      return nums.sum 
-    end
+    nums = numbers.split(/,|\n/).map(&:to_i)
+    nums.sum
   end
 end
