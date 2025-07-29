e1696461fe9bf5e77ca6def3200660a385d2b405
slavanya <slavanya@plansource.com>
2025-07-29 13:54:26 +0530

    Add failing test for: single number input

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -7,5 +7,10 @@ describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(calc.add('')).to eq(0)
     end
+
+    it 'returns the number for a single number string' do
+      expect(calc.add('1')).to eq(1)
+      expect(calc.add('42')).to eq(42)
+    end
   end
 end
