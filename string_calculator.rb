c188b11037d83b94ba1e8254ee1b031ef50b50c6
slavanya <slavanya@plansource.com>
2025-07-29 14:03:03 +0530

    Implement: Two numbers sum

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,5 +2,9 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers == ""
     return numbers.to_i if numbers =~ /^\d+$/
+    if numbers.include?(',')
+      nums = numbers.split(',').map(&:to_i)
+      return nums[0] + nums[1]
+    end
   end
 end
